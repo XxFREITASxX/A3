32a545e8dc3339d8c067eca71d862af0258bb120
Gabriel de Freitas <gfmonguilhott793@gmail.com>
2023-06-18 08:27:10 -0300

    okok

--- A3 Teste/estoque.cpp
+++ A3 Teste/estoque.cpp
@@ -13,8 +13,8 @@ void Estoque::adicionarProduto() {
 	cout << "\nID do produto: ";
 	cin >> id;
 
-	for (const auto& produto : produtos) {
-		if (produto.getId() == id) { // Checa se já existe um produto com o id
+	for (const auto& produto : produtos) { // Percorre os produtos que estao no vetor de produtos
+		if (produto.getId() == id) {       // Checa se já existe um produto cadastrado com esse ID
 			cout << "\nJa existe um produto com este ID cadastrado. Porfavor, escolha outro ID.";
 			return;
 		}
@@ -40,17 +40,17 @@ void Estoque::adicionarProduto() {
 	cout << "Quantidade do produto: ";
 	cin >> quantidade;
 
-	Produto produto(id, nome, preco, tamanho, cor, marca, quantidade);
-	produtos.push_back(produto);
+	Produto produto(id, nome, preco, tamanho, cor, marca, quantidade);  // Cria o objeto do tipo produto e passa os valores fornecidos como argumentos para o construtor 'Produto'
+	produtos.push_back(produto);									    // Adiciona o objeto 'produto' ao final do vetor de produtos
 
 	cout << "\nProduto adicionado ao estoque!\n";
 }
 
 void Estoque::removerProduto(int id) {
 	bool produtoRemovido = false;
-	for (auto it = produtos.begin(); it != produtos.end(); ++it) {
-		if (it->getId() == id){
-		produtos.erase(it);
+	for (auto it = produtos.begin(); it != produtos.end(); ++it) { // Aponta o primeiro e ultimo elemento do vetor, se o iterador 'it' não chegou no final do vetor, incrimenta o iterador com ++it, colocando-o no próximo elemento do vetor
+		if (it->getId() == id){									   // Verifica se o ID do produto é igual ao fornecido
+		produtos.erase(it);										   // Remove o produto
 		produtoRemovido = true;
 		break;
 	}
